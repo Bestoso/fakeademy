4413d66ae2f03a01a29bf6b10477ef3a03db297f
Santiago <santiaguitobestoso@gmail.com>
2023-12-09 01:04:50 -0300

    fix mp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,14 @@ import { useEffect } from "react";
 import { RoutesComponent } from "./components/RoutesComponent";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { initMercadoPago } from '@mercadopago/sdk-react';
 
 function App (){
+
+  useEffect(() => {
+    initMercadoPago('APP_USR-f4ee0560-b1b6-433b-80f8-397a12d4d84b');
+  }, []);
+
   useEffect(() => {
     window.document.title = "Coderhouse";
     AOS.init({
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './assets/css/index.css'
-import { initMercadoPago } from '@mercadopago/sdk-react';
-
-initMercadoPago('APP_USR-f4ee0560-b1b6-433b-80f8-397a12d4d84b');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
